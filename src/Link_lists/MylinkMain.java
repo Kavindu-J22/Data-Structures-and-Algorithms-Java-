ac26411ba84b03a8b57e12bb6bb8947cd71e0f38
Kavindu-J22 <it21032806@my.sliit.lk>
2022-11-25 01:29:11 +0530

    List main upload
    update cord

--- src/Link_lists/MylinkMain.java
+++ src/Link_lists/MylinkMain.java
@@ -1,5 +1,27 @@
 package Link_lists;
 
 public class MylinkMain {
+	public static void main(String[] args) {
+		linkList lk1 = new linkList();
+		
+		lk1.insertFrist(10);
+		lk1.insertFrist(20);
+		lk1.insertFrist(30);
+		lk1.insertFrist(40);
+		lk1.insertFrist(50);
+		
+		lk1.DisplayList();
+		
+		System.out.println(" "); // for space
+		
+		while(!lk1.isempty()) {
+			linkx val = lk1.DeletFrist();
+			System.out.print("Deleted - ");
+			val.displayData();
+		}
+		
+		lk1.DisplayList();
+			
+	}
 
 }
